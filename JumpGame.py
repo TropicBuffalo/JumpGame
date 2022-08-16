664ec623916d94bebe2fab8b2de046ef2412e1af
TropicBuffalo <fannzio@gmail.com>
2022-08-16 13:34:45 +0000

    Update JumpGame.py

--- JumpGame.py
+++ JumpGame.py
@@ -17,12 +17,15 @@ spike = pygame.Surface((25,50))
 spike.fill((0,0,0))
 spike_x_pos = 800
 
+
+
 player = pygame.Surface((25,25))
 player.fill((0,0,0))
 player_rect = player.get_rect(topleft=(100,275))
 player_gravity = 0
 player_y = 200
 
+screen.blit(ground,(0,300))
 
 while True:
 	for event in pygame.event.get():
@@ -30,30 +33,32 @@ while True:
 			pygame.quit()
 			exit()
 
-	screen.blit(ground,(0,300))
+	#Need to redraw the sky because of spikes
 	screen.blit(sky,(0,0))
 
-	spike_x_pos -= 2
-	if spike_x_pos < -25 : spike_x_pos = 800
-	screen.blit(spike,(spike_x_pos,250))
+	#Spike
+	spike_x_pos -= 4
 	spike_rect = spike.get_rect(topleft=(spike_x_pos,250))
+	if spike_x_pos < -25 : spike_x_pos = 800
 
+	#Redraw spike when its out of the screen
+	if spike_rect.right <= 0: spike_rect.left = 800
+	screen.blit(spike,spike_rect)
+	
+	#Player
 	player_gravity += 1
 	player_rect.y += player_gravity
+	if player_rect.bottom >= 300: player_rect.bottom = 300
 	screen.blit(player,player_rect)
-
-
 
 
 	if player_rect.colliderect(spike_rect):
 		print('Collision')
 
-
-	key = pygame.key.get_pressed()
-	if key[pygame.K_SPACE]:
-		player_gravity = -20
-		print(player_rect.bottom)
-
+	#Keyboard input
+	if event.type ==pygame.KEYDOWN:
+		if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+			player_gravity = -18
 
 
 	pygame.display.update()
